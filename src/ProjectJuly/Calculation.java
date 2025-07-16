2a4e5f6a770d7d1c1763bb94496914fd76c3dc59
hamidie ahmat <ahmathisseinh@gmail.com>
2025-07-16 16:46:32 +0100

    hello world and calculator commits

---
+++ src/ProjectJuly/Calculation.java
@@ -0,0 +1,12 @@
+package ProjectJuly;
+
+import java.sql.SQLOutput;
+
+public class Calculation {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 3;
+        int sum = a + b;
+        System.out.println("Sum is: " + sum);
+    }
+}
---
+++ src/ProjectJuly/PJuly.java
@@ -0,0 +1,9 @@
+package ProjectJuly;
+
+public class PJuly {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+    }
+}
