dfeab9424c1517889eb1d276aa3636d44f30c50c
hamidie ahmat <ahmathisseinh@gmail.com>
2025-07-17 20:48:50 +0100

    for loop structure

---
+++ src/ProjectJuly/ForLoops.java
@@ -0,0 +1,12 @@
+package ProjectJuly;
+
+public class ForLoops {
+    public  static void  main ( String[] args){
+        int number=5 ;
+        // for loop control flow
+        for(int i=0 ;i <= number;i++){
+            System.out.println("for executes until the condition is true !");
+        }
+        System.out.println("outside loop !");
+    }
+}
