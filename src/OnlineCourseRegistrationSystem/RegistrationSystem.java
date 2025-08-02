a5d20d26b5ece0ac151446c088542192021144f9
hamidie ahmat <ahmathisseinh@gmail.com>
2025-08-02 11:29:15 +0100

    Simulate a basic online system where a student can register for courses.

--- src/OnlineCourseRegistrationSystem/RegistrationSystem.java
+++ src/OnlineCourseRegistrationSystem/RegistrationSystem.java
@@ -4,19 +4,18 @@ public class RegistrationSystem {
     public static void main(String[]args){
         Student s=new Student("Abdelhamid",4);
         Student s1=new Student("wardougou",5);
-        Student s2 =new Student("rozi",7);
+
 
         Course c=new Course("computer ingineering","p342");
         Course c1=new Course("civil engineering","p352");
-        Course c2=new Course("Medecine","M532");
+
 
         Registration r=new Registration(s,c);
         Registration r1=new Registration(s1,c1);
-        Registration r2=new Registration(s2,c2);
 
         r.displayRegistrationInfo();
         r1.displayRegistrationInfo();
-        r2.displayRegistrationInfo();
+
 
 
     }
